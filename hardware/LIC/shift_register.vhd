2883b62e9d6e6363fd412558854ab6d6f6cfb7b2
awyxx <robyawyx@gmail.com>
2022-03-14 22:15:46 +0000

    shift register half fix

---
+++ hardware/LIC/shift_register.vhd
@@ -0,0 +1,142 @@
+-- Shift Register Testbench
+
+LIBRARY IEEE;
+USE IEEE.std_logic_1164.ALL;
+
+ENTITY shift_register IS
+	PORT (
+		Sin : IN std_logic;
+		clk, enable : IN STD_LOGIC;
+		D : OUT STD_LOGIC_VECTOR(9 DOWNTO 0)
+	);
+END shift_register;
+
+ARCHITECTURE arq OF shift_register IS
+
+	COMPONENT FFD IS
+		PORT (
+			CLK : IN STD_LOGIC;
+			RESET : IN STD_LOGIC;
+			SET : IN STD_LOGIC;
+			D : IN STD_LOGIC;
+			EN : IN STD_LOGIC;
+			Q : OUT STD_LOGIC
+		);
+	END COMPONENT;
+ 
+	SIGNAL q : std_logic_vector(9 DOWNTO 0);
+
+BEGIN
+
+	u_ffd0 : FFD
+	PORT MAP(
+		EN => enable, 
+		RESET => '0', 
+		SET => '0', 
+		CLK => clk, 
+		D => Sin, 
+		Q => q(0)
+	);
+ 
+	u_ffd1 : FFD
+	PORT MAP(
+		EN => enable, 
+		RESET => '0', 
+		SET => '0', 
+		CLK => clk, 
+		D => q(0), 
+		Q => q(1)
+	);
+ 
+	u_ffd2 : FFD
+	PORT MAP(
+		EN => enable, 
+		RESET => '0', 
+		SET => '0', 
+		CLK => clk, 
+		D => q(1), 
+		Q => q(2)
+	);
+ 
+	u_ffd3 : FFD
+	PORT MAP(
+		EN => enable, 
+		RESET => '0', 
+		SET => '0', 
+		CLK => clk, 
+		D => q(2), 
+		Q => q(3)
+	);
+ 
+	u_ffd4 : FFD
+	PORT MAP(
+		EN => enable, 
+		RESET => '0', 
+		SET => '0', 
+		CLK => clk, 
+		D => q(3), 
+		Q => q(4)
+	);
+ 
+	u_ffd5 : FFD
+	PORT MAP(
+		EN => enable, 
+		RESET => '0', 
+		SET => '0', 
+		CLK => clk, 
+		D => q(4), 
+		Q => q(5)
+	);
+ 
+	u_ffd6 : FFD
+	PORT MAP(
+		EN => enable, 
+		RESET => '0', 
+		SET => '0', 
+		CLK => clk, 
+		D => q(5), 
+		Q => q(6)
+	);
+ 
+	u_ffd7 : FFD
+	PORT MAP(
+		EN => enable, 
+		RESET => '0', 
+		SET => '0', 
+		CLK => clk, 
+		D => q(6), 
+		Q => q(7)
+	);
+ 
+	u_ffd8 : FFD
+	PORT MAP(
+		EN => enable, 
+		RESET => '0', 
+		SET => '0', 
+		CLK => clk, 
+		D => q(7), 
+		Q => q(8)
+	);
+ 
+	u_ffd9 : FFD
+	PORT MAP(
+		EN => enable, 
+		RESET => '0', 
+		SET => '0', 
+		CLK => clk, 
+		D => q(8), 
+		Q => q(9)
+	);
+
+	D(0) <= q(0) AND NOT(enable);
+	D(1) <= q(1) AND NOT(enable);
+	D(2) <= q(2) AND NOT(enable);
+	D(3) <= q(3) AND NOT(enable);
+	D(4) <= q(4) AND NOT(enable);
+	D(5) <= q(5) AND NOT(enable);
+	D(6) <= q(6) AND NOT(enable);
+	D(7) <= q(7) AND NOT(enable);
+	D(8) <= q(8) AND NOT(enable);
+	D(9) <= q(9) AND NOT(enable);
+	
+END arq;
---
+++ hardware/LIC/shift_register_tb.vhd
@@ -0,0 +1,87 @@
+-- Shift Register Testbench
+
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+
+ENTITY shift_register_tb IS
+END shift_register_tb;
+
+ARCHITECTURE arq OF shift_register_tb IS
+
+	COMPONENT shift_register IS
+		PORT (
+			Sin : IN STD_LOGIC;
+			clk, enable : IN STD_LOGIC;
+			D : OUT STD_LOGIC_VECTOR(9 DOWNTO 0)
+		);
+	END COMPONENT;
+
+	CONSTANT MCLK_PERIOD : TIME := 20 ns;
+	CONSTANT MCLK_HALF_PERIOD : TIME := MCLK_PERIOD / 2;
+
+	SIGNAL Sin_tb : STD_LOGIC;
+	SIGNAL clk_tb, enable_tb : STD_LOGIC;
+	SIGNAL D_tb : STD_LOGIC_VECTOR(9 DOWNTO 0);
+
+BEGIN
+	-- Unit Under Test
+	UUT : shift_register
+	PORT MAP(
+		Sin => Sin_tb, 
+		clk => clk_tb, 
+		enable => enable_tb, 
+		D => D_tb
+	);
+
+	-- Generate clock
+	clk_gen : PROCESS
+	BEGIN
+		clk_tb <= '0';
+		WAIT FOR MCLK_HALF_PERIOD;
+		clk_tb <= '1';
+		WAIT FOR MCLK_HALF_PERIOD;
+	END PROCESS;
+
+	-- Stimulus generator
+	stimulus : PROCESS
+	BEGIN
+		enable_tb <= '1';
+		WAIT FOR MCLK_HALF_PERIOD * 2;
+		
+		Sin_tb <= '1';
+		WAIT FOR MCLK_HALF_PERIOD * 2;
+ 
+		Sin_tb <= '1';
+		WAIT FOR MCLK_HALF_PERIOD * 2;
+ 
+		Sin_tb <= '0';
+		WAIT FOR MCLK_HALF_PERIOD * 2;
+ 
+		Sin_tb <= '1';
+		WAIT FOR MCLK_HALF_PERIOD * 2;
+ 
+		Sin_tb <= '1';
+		WAIT FOR MCLK_HALF_PERIOD * 2;
+ 
+		Sin_tb <= '0';
+		WAIT FOR MCLK_HALF_PERIOD * 2;
+ 
+		Sin_tb <= '1';
+		WAIT FOR MCLK_HALF_PERIOD * 2;
+ 
+		Sin_tb <= '0';
+		WAIT FOR MCLK_HALF_PERIOD * 2;
+ 
+		Sin_tb <= '1';
+		WAIT FOR MCLK_HALF_PERIOD * 2;
+ 
+		Sin_tb <= '1';
+		WAIT FOR MCLK_HALF_PERIOD * 2;
+ 
+		enable_tb <= '0';
+		WAIT FOR MCLK_HALF_PERIOD * 2;
+ 
+		WAIT;
+	END PROCESS;
+
+END arq;
