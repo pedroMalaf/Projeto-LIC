40365b074cafe5c53792459f353d7236a87c9ae6
roby2014 <robyawyx@gmail.com>
2022-04-11 09:39:41 +0100

    added HAL.kt

---
+++ software/lic/src/main/kotlin/HAL.kt
@@ -0,0 +1,31 @@
+object HAL { // Virtualiza o acesso ao sistema UsbPort
+    // Inicia a classe
+    fun init() {
+
+    }
+
+    // Retorna true se o bit tiver o valor lógico ‘1’
+    fun isBit(mask: Int): Boolean {
+
+    }
+
+    // Retorna os valores dos bits representados por mask presentes no UsbPort
+    fun readBits(mask: Int): Int {
+
+    }
+
+    // Escreve nos bits representados por mask o valor de value
+    fun writeBits(mask: Int, value: Int) {
+
+    }
+
+    // Coloca os bits representados por mask no valor lógico ‘1’
+    fun setBits(mask: Int) {
+
+    }
+
+    // Coloca os bits representados por mask no valor lógico ‘0’
+    fun clrBits(mask: Int) {
+
+    }
+}
