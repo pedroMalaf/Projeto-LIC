0ca59ddde30458cdf5fe9fc2e4c33ca6cddf4b1e
awyxx <robyawyx@gmail.com>
2022-04-11 09:26:01 +0100

    HAL

--- software/lic/src/main/kotlin/Main.kt
+++ software/lic/src/main/kotlin/Main.kt
@@ -1,9 +1,7 @@
 import isel.leic.UsbPort
 
 fun main(args: Array<String>) {
-    println("Hello World!")
-
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    val value = UsbPort.read()
+    println(value)
+    UsbPort.write(value)
 }
