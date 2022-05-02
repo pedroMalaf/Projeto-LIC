5fb8a882c20f5c45d0fec0a2d2d2dee418c768ad
roby2014 <robyawyx@gmail.com>
2022-05-02 11:34:31 +0100

    TicketDispenser fixes

--- software/lic/src/main/kotlin/TicketDispenser.kt
+++ software/lic/src/main/kotlin/TicketDispenser.kt
@@ -1,17 +1,25 @@
+fun main() {
+    // 0010 0001 1
+    TestbenchTicketDispenser(1, 2, true)
+}
+
+// Cria uma trama (bilhete) com o destino, origem e round trip
 object TicketDispenser {
     fun init() {
-        serialEmitter.init()
+        SerialEmitter.init()
     }
 
+    // Envia uma trama recorrendo ao SerialEmitter
     fun print(destinyID: Int, originID: Int, roundTrip: Boolean) {
-
-        var newRT = 0
-        if( roundTrip == false ) newRT = 0 else newRT = 1
-
-        val data = newRT or destinyID.shl(1) or originID.shl(5)
-
-        serialEmitter.send(TICKET_DISPENSER, data)
+        val RT = if (!roundTrip) 0 else 1
+        val data = RT or destinyID.shl(1) or originID.shl(5)
+        SerialEmitter.send(SerialEmitter.Destination.TICKER_DISPENSER, data)
     }
+}
+
+fun TestbenchTicketDispenser(d: Int, o: Int, rt: Boolean) {
+    TicketDispenser.init()
+    TicketDispenser.print(d, o, rt)
 }
 
 
