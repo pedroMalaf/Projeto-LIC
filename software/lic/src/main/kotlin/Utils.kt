dafccb72f15bd1c84d13e73939d0c6db3180fd0a
roby2014 <robyawyx@gmail.com>
2022-05-02 11:34:43 +0100

    Utils (printNum) fixes

--- software/lic/src/main/kotlin/Utils.kt
+++ software/lic/src/main/kotlin/Utils.kt
@@ -8,6 +8,5 @@ fun isPowerOfTwo(n: Int) = (n and (n-1)) == 0
 /**
  * Prints [n] in decimal, hex and binary.
  */
-fun printNum(n: Int) {
-    println("%d (0x%x) (0b%s)".format(n, n, n.toString(2)))
-}
+fun printNum(n: Int) =
+    "%d (0x%x) (0b%s)".format(n, n, n.toString(2))
