d3b6064b1f3abf35b38168573529e3a095e5b959
roby2014 <robyawyx@gmail.com>
2022-05-17 18:56:21 +0100

    parity bit fix, added debug messages and english documentation

--- software/lic/src/main/kotlin/SerialEmitter.kt
+++ software/lic/src/main/kotlin/SerialEmitter.kt
@@ -5,83 +5,95 @@ fun main() {
 }
 
 /**
- * Serial Emitter
+ * SerialEmitter
  *
- * Envia tramas para os diferentes módulos Serial Receiver
+ * Sends the frame to the SerialReceiver module (IOS)
  */
 object SerialEmitter {
-    enum class Destination { LCD, TICKER_DISPENSER }
+    enum class Destination(val value: Int, val to: String) {
+        LCD(0, "LCD"),
+        TICKER_DISPENSER(1, "TICKET_DISPENSER")
+    }
 
-    // Canais:
+    // bit channels
     private const val BUSY_MASK = 0x1 // bit 0
     private const val NOTSS_MASK = 0x2 // 1
     private const val SCLK_MASK = 0x4 // 2
     private const val SDX_MASK = 0x8 // 3
 
-    const val SCLK = 5L
-    const val SLEEP_DEBUG = 500L
+    //
+    private const val SCLK = 5L
+    private const val SLEEP_DEBUG = 500L
 
+    /**
+     * Sets up bit channels
+     */
     fun init() {
-        // busy = 0
-        HAL.clrBits(BUSY_MASK)
+        DEBUG("[SerialEmitter::init]")
 
-        // not_ss = 1
-        HAL.setBits(NOTSS_MASK)
+        HAL.clrBits(BUSY_MASK) // busy = 0
+        HAL.setBits(NOTSS_MASK) // not_ss = 1
     }
 
-    // Envia uma trama para o SerielReceiver identificado o destino em addr e os bits de dados em data
+    /**
+     * Sends the 11bit frame to SerielReceiver hardware module.
+     *
+     * The frame should look like this: originID[6..9] destinyID[2..5] rt[1] lcd_td[0]
+     * (+ the last bit sent which is the parity bit)
+     *
+     * @param addr Where to send the frame to (LCD or TICKET_DISPENSER)
+     * @param data originID[5..8] destinyID[1..4] rt[0]
+     */
     fun send(addr: Destination, data: Int) {
-        while(SerialEmitter.isBusy()){
-            println("Não é possível enviar uma trama. Tenta novamente em 3 segundos")
+        while (isBusy()) {
+            DEBUG("Cant send frame (busy = 1). Trying again in 3 sec ...")
             Time.sleep(3000)
         }
 
-        // data = origem destino rt (ex: 67 = 0010 0001 1)
-        val d = if (addr == Destination.TICKER_DISPENSER) 1 else 0
+        // add destination bit (lcd or td) to frame (msb)
+        var frame = data.shl(1) or addr.value
+        DEBUG("[SerialEmitter::send] sending frame ${AS_BINARY(frame)} (${addr.to})")
 
-        // adicionar bit de destination (lcd ou td) ao data (bit de maior peso)
-        //var fullSdx = d.shl(9) or data
-        var fullSdx = data.shl(1) or d
-        // iterar e variar/enviar SCLK e SDX (um de cada vez)
+        var parityBit = 0
+
+        // send frame (bit by bit individually)
         HAL.clrBits(NOTSS_MASK)
-        for (i in 0 until 9) {
-            println("Serial Emitter sending: ${printNum(fullSdx)}")
-            // sclk = 0
-            HAL.clrBits(SCLK_MASK)
-
+        repeat(10) {
+            HAL.clrBits(SCLK_MASK) // sclk = 0
             Time.sleep(SCLK)
+            HAL.setBits(SCLK_MASK) // sclk = 1
 
-            // sclk = 1
-            HAL.setBits(SCLK_MASK)
-
-            // sdx
-            val sdx = fullSdx and 1
+            // sdx and parity
+            val sdx = frame and 1
             if (sdx == 1) HAL.setBits(SDX_MASK) else HAL.clrBits(SDX_MASK)
+            parityBit = parityBit xor sdx
+            DEBUG("[SerialEmitter::send] sdx: $sdx (parityBit = $parityBit)")
+            frame = frame.shr(1)
 
-            fullSdx = fullSdx.shr(1)
             Time.sleep(SLEEP_DEBUG)
         }
 
+        // send last bit (parity)
+        HAL.clrBits(SCLK_MASK) // sclk = 0
+        Time.sleep(SCLK)
+        HAL.setBits(SCLK_MASK) // sclk = 1
+        DEBUG("[SerialEmitter::send] sending last sdx (parity bit): $parityBit")
+
+        Time.sleep(SCLK)
         HAL.setBits(BUSY_MASK)
         HAL.setBits(NOTSS_MASK)
         HAL.clrBits(SCLK_MASK)
+        DEBUG("[SerialEmitter::send] end frame")
     }
 
-    // Retorna true se o canal série estiver ocupado
-    fun isBusy(): Boolean {
-        return HAL.isBit(BUSY_MASK)
-    }
+    /**
+     * Returns true if busy channel is ON
+     */
+    fun isBusy() = HAL.isBit(BUSY_MASK)
 }
 
 fun SerialEmitterTestbench(data: Int) {
-    // README: if testing on real board, make sure to manipulate switches
+    // README: if testing on real board, make sure to manipulate switches (?)
     SerialEmitter.init()
-
-    if (SerialEmitter.isBusy())
-        println("isBusy should start at false!")
-
     SerialEmitter.send(SerialEmitter.Destination.TICKER_DISPENSER, data)
-
-    if (!SerialEmitter.isBusy())
-        println("isBusy should return true!")
 }
