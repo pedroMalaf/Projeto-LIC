23758d41cd5bd2e9cdc44abb8e12a65ba9355c25
roby2014 <robyawyx@gmail.com>
2022-06-08 00:46:53 +0100

    added testbenched for `M` class

--- software/lic/src/main/kotlin/M.kt
+++ software/lic/src/main/kotlin/M.kt
@@ -1,12 +1,34 @@
+import isel.leic.utils.Time
+import kotlin.test.assertEquals
+
+/**
+ * Maintenance.
+ */
 object M {
 
     fun init() {
         HAL.init()
     }
 
-    fun verify(): Boolean {
-        val ver = HAL.isBit(0b1000_0000)
-        return ver
-    }
+    /**
+     * Returns true if in maintenance mode.
+     */
+    fun verify() = HAL.isBit(M_MASK)
 }
 
+fun main() {
+    M_Testbench()
+}
+
+fun M_Testbench() {
+    DEBUG("[M::TESTBENCH] starting")
+    M.init()
+
+    assertEquals(M.verify(), false)
+
+    DEBUG("[M::TESTBENCH] set M to 1 (I7)")
+    Time.sleep(5000)
+    assertEquals(M.verify(), true)
+
+    DEBUG("[M::TESTBENCH] done")
+}
