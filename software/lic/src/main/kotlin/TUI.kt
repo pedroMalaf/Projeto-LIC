96e0640fe42b597e58f316b260c7cf5e1262baa8
roby2014 <robyawyx@gmail.com>
2022-06-08 00:47:15 +0100

    added base testbench format for `TUI` class

--- software/lic/src/main/kotlin/TUI.kt
+++ software/lic/src/main/kotlin/TUI.kt
@@ -1,6 +1,4 @@
 object TUI {
-
-
     fun init() {
         KeyReceiver.init()
         LCD.init()
@@ -9,4 +7,14 @@ object TUI {
     fun writeLCD() {
         LCD.write(KBD.getKey())
     }
+}
+
+fun main() {
+    TUI_Testbench()
+}
+
+fun TUI_Testbench() {
+    DEBUG("[TUI::TESTBENCH] starting")
+    TODO()
+    DEBUG("[TUI::TESTBENCH] done")
 }
