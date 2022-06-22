593a1e06047a8756aa84e0941cb9534e12b3a08e
roby2014 <robyawyx@gmail.com>
2022-06-22 01:22:42 +0100

    implemented maintencemode screen and removed debug messages to avoid spam

--- software/lic/src/main/kotlin/TicketMachine.kt
+++ software/lic/src/main/kotlin/TicketMachine.kt
@@ -1,14 +1,21 @@
+import isel.leic.utils.Time
 import java.text.SimpleDateFormat
 import java.util.*
 
 val DATEFORMAT = SimpleDateFormat("dd/MM/yyyy HH:mm")
 
+var waitingScreenDisplayed = false
+
 fun showWaitingScreen() {
-    LCD.clear()
-    LCD.cursor(0, 1)
-    LCD.write("Ticket to Ride")
-    LCD.newLine()
-    LCD.write(DATEFORMAT.format(Date()))
+    if (!waitingScreenDisplayed) {
+        LCD.clear()
+        LCD.cursor(0, 1)
+        LCD.write("Ticket to Ride")
+        LCD.newLine()
+        LCD.write(DATEFORMAT.format(Date()))
+        waitingScreenDisplayed = true
+    }
+
 }
 
 /**
@@ -37,6 +44,7 @@ object TicketMachine {
         // main loop
         while (!shutdown) {
             if (M.verify()) {
+                waitingScreenDisplayed = false
                 shutdown = MaintenceMode()
             }
             if (KBD.getKey() == '#') {
@@ -49,19 +57,47 @@ object TicketMachine {
 }
 
 fun MaintenceMode(): Boolean {
+    val options = listOf(
+        "1-Print Ticket",
+        "2-Station Cnt.",
+        "3-Coins Cnt.",
+        "4-Reset Cnt.",
+        "5-Shutdown"
+    )
+    var idx = 0
+    var timeNow = Time.getTimeInMillis()
+
     LCD.clear()
     LCD.cursor(0, 1)
     LCD.write("Maintence mode")
 
     do {
+        if (elapsed(timeNow) > 2500) {
+            if (idx == options.size-1) idx = 0
+            else idx++
+            timeNow = Time.getTimeInMillis()
+            LCD.clear()
+            LCD.cursor(0, 1)
+            LCD.write("Maintence mode") // TODO: always clearing
+        }
+
+        LCD.cursor(1, 0)
+        LCD.write(options[idx])
+
         when (KBD.getKey()) {
-            '1' -> println("1")
+            '1' -> {
+
+            }
             '2' -> println("2")
             '3' -> println("3")
         }
     } while (M.verify())
 
     return false
+}
+
+fun elapsed(t: Long): Long {
+    return Time.getTimeInMillis() - t
 }
 
 fun main() {
