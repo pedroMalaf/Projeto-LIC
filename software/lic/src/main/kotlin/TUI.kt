018ff5a968926d8374a7bf69812d4c1bb49ff869
roby2014 <robyawyx@gmail.com>
2022-06-25 02:27:52 +0100

    fixed a lot of code in TUI and APP to respect "inheritance" mode from pdf

--- software/lic/src/main/kotlin/TUI.kt
+++ software/lic/src/main/kotlin/TUI.kt
@@ -1,21 +1,14 @@
 import isel.leic.utils.Time
-import java.text.SimpleDateFormat
-import java.util.*
 
+// EURO + ARROW SPECIAL CHARS
+const val EURO_CHAR = 'e' //TODO:
+const val ARROW_CHAR = 'a' // TODO:
+
+/**
+ * TUI
+ * Contains functions to help us draw/handle things on LCD
+ */
 object TUI {
-    // EURO + ARROW SPECIAL CHARS
-    private const val EURO_CHAR = 'e' //TODO:
-    private const val ARROW_CHAR = 'a' // TODO:
-
-    // Used to know when to redraw waiting message
-    var updateWaitingScreen = false
-
-    // Our current city
-    private var origin = 0
-
-    // Updated date + format
-    private val DATEFORMAT = SimpleDateFormat("dd/MM/yyyy HH:mm")
-    private var updatedDate = DATEFORMAT.format(Date())
 
     /**
      * Inits TUI
@@ -45,6 +38,14 @@ object TUI {
     }
 
     /**
+     * Writes the "* - to print* to LCD.
+     */
+    fun toPrintText(rt: Boolean) {
+        LCD.newLine()
+        LCD.write("${if (rt) 1 else 0}  * to Print")
+    }
+
+    /**
      * Clears and writes [s] on the LCD on cursor ([l], [c]) or centered instead
      */
     fun clearAndWrite(s: String, center: Boolean = false, l: Int = 0, c: Int = 0, clear: Boolean = true) {
@@ -59,322 +60,63 @@ object TUI {
     }
 
     /**
-     * Shows waiting screen on LCD
+     * Writes on the LCD:
+     *    city
+     *  rt price
      */
-    fun showWaitingScreen() {
-        val now = DATEFORMAT.format(Date())
-        if (now != updatedDate) {
-            updatedDate = now
-            updateWaitingScreen = false
-        }
-        if (!updateWaitingScreen) {
-            clearAndWrite("Ticket To Ride", true)
-            LCD.newLine()
-            LCD.write(updatedDate.toString())
-            updateWaitingScreen = true
-        }
-    }
-
-    /**
-     * Handles city selection and executes [fn] block.
-     * The function returns if [returnIfHashtag] is true and '#' is pressed.
-     * [showPrice] will show the price in the right side, if false, shows how many tickets were sold.
-     * This can be useful to avoid duplicate code.
-     * Lambda char is key pressed, String is city object, Int is city ID
-     */
-    private fun handleCitySelection(
-        returnIfHashtag: Boolean, showPrice: Boolean, fn: (Char, Stations.City, Int) -> Unit
-    ) {
-        val cities = Stations.cities
-        var keyIdx = 0
-        var arrowMode = false
-
-        // Writes to LCD city correctly formatted
-        fun writeCity() {
-            clearAndWrite(cities[keyIdx].name, true)
-            LCD.newLine()
-            val str = String.format("%02d${if (arrowMode) ARROW_CHAR else ":"}", keyIdx)
-            LCD.write(str)
-            if (!showPrice) {
-                LCD.cursor(1, 14)
-                LCD.write(String.format("%02d", cities[keyIdx].sold))
-            } else {
-                LCD.cursor(1, 11)
-                val price = centsToString(cities[keyIdx].price)
-                LCD.write("$price$EURO_CHAR")
-            }
-        }
-
-        // loop and show cities
-        writeCity()
-        while (true) {
-            val key = KBD.waitKey(5000)
-            if (key == KBD.NONE.toChar()) {
-                break
-            }
-
-            if (key == '#' && returnIfHashtag)
-                break
-
-            if (key == '*') {
-                arrowMode = !arrowMode
-                writeCity()
-                continue
-            }
-
-            // handle arrow mode
-            if ((key == '2' || key == '8') && arrowMode) {
-                val up = key == '2'
-                when {
-                    up && keyIdx != cities.size - 1 -> keyIdx++
-                    !up && keyIdx > 0 -> keyIdx--
-                    else -> continue
-                }
-                writeCity()
-                continue
-            }
-
-            // handle normal mode
-            if ((key - '0') < cities.size && key != '#') {
-                // check if we can "autocomplete" 2 digit
-                // i.e press 1 and then 2 gets us 12, but if 12 does not exist, gets 2
-                val n = String.format("%d%d", keyIdx, key - '0').toInt()
-                keyIdx = if (n < cities.size)
-                    n
-                else
-                    key - '0'
-                writeCity()
-            }
-
-            fn(key, cities[keyIdx], keyIdx)
-        }
-    }
-
-    /**
-     * Maintenance mode screen
-     */
-    fun maintenanceMode(): Boolean {
-        val options = listOf(
-            "1-Print Ticket",
-            "2-Station Cnt.",
-            "3-Coins Cnt.",
-            "4-Reset Cnt.",
-            "5-Shutdown"
+    fun writeCityRtPrice(cityName: String, rt: Boolean, price: Int) {
+        clearAndWrite(cityName, true)
+        clearAndWrite(
+            "${if (rt) 1 else 0}    ${centsToString(price)}$EURO_CHAR",
+            l = 1,
+            clear = false
         )
-        var idx = 0
-        var timer = Time.getTimeInMillis()
-
-        clearAndWrite("Maintenance mode")
-
-        do {
-            if (elapsed(timer) > 2500) {
-                if (idx == options.size - 1) idx = 0
-                else idx++
-                clearAndWrite("Maintenance mode")
-                timer = Time.getTimeInMillis()
-            }
-
-            LCD.cursor(1, 0)
-            LCD.write(options[idx])
-
-            when (KBD.getKey()) {
-                '1' -> testPrintTicket()
-                '2' -> maintenanceStationsScreen()
-                '3' -> maintenanceCoinsScreen()
-                '4' -> { // reset
-                    if (yesNoQuestion("Reset counters?", 5000L)) {
-                        Stations.resetStations()
-                        CoinDeposit.resetQuantity()
-                    }
-                }
-                '5' -> { // shutdown
-                    if (yesNoQuestion("Shutdown?", 5000L)) return true
-                    else continue
-                }
-            }
-        } while (M.verify())
-        return false
     }
 
     /**
-     * Maintenance coins screen
+     * Writes on LCD:
+     *         value
+     * idx arr       quantity
      */
-    private fun maintenanceCoinsScreen() {
-        val coins = CoinDeposit.coins.keys.toList()
-        var arrowMode = false
-        var keyIdx = 0
-        val toCent = listOf(5, 10, 20, 50, 100, 200)
-
-        // Display coins info in the correct format
-        fun writeCoins() {
-            clearAndWrite("${centsToString(toCent[keyIdx])}$EURO_CHAR", true)
-            LCD.newLine()
-            LCD.write("$keyIdx${if (arrowMode) ARROW_CHAR else ":"}")
-            LCD.cursor(1, 14)
-            LCD.write(String.format("%02d", CoinDeposit.coins[coins[keyIdx]]))
-        }
-
-        writeCoins()
-        while (true) {
-            val key = KBD.waitKey(5000)
-            if (key == KBD.NONE.toChar() || key == '#') {
-                break
-            }
-
-            if (key == '*') {
-                arrowMode = !arrowMode
-                writeCoins()
-                continue
-            }
-
-            // handle arrow mode
-            if ((key == '2' || key == '8') && arrowMode) {
-                val up = key == '2'
-                when {
-                    up && keyIdx < 5 -> keyIdx++
-                    !up && keyIdx > 0 -> keyIdx--
-                    else -> continue
-                }
-                writeCoins()
-                continue
-            }
-
-            // handle normal mode
-            if (key in '0'..'5') {
-                keyIdx = key - '0'
-                writeCoins()
-            }
-        }
+    fun writeCoins(value: Int, quant: Int, arrowMode: Boolean, idx: Int) {
+        clearAndWrite("${centsToString(value)}$EURO_CHAR", true)
+        LCD.newLine()
+        LCD.write("$idx${if (arrowMode) ARROW_CHAR else ":"}")
+        LCD.cursor(1, 14)
+        LCD.write(String.format("%02d", quant))
     }
 
     /**
-     * Maintenance stations screen
+     * Shows vending aborted screen.
+     * Lambda is runned after (to get return value and eject coins)
+     * Lambda int parameter is credit
      */
-    private fun maintenanceStationsScreen() {
-        handleCitySelection(true, false) { _, _, _ -> }
+    fun vendingAbortedScreen(credit: Int, fn: (Int) -> Unit) {
+        clearAndWrite("Vending aborted", true)
+        clearAndWrite(
+            "Return ${centsToString(credit)}$EURO_CHAR",
+            true,
+            l = 1,
+            clear = false
+        )
+        fn(credit)
+        Time.sleep(1000)
+
     }
 
     /**
-     * Simulates ticket printing (maintenance mode)
+     * Shows collecting ticket screen.
+     * Lambda will be used to handle the ticket purchase logic.
      */
-    private fun testPrintTicket() {
-        handleCitySelection(false, true) { key, city, id ->
-            if (key == '#') {
-                var rt = false
-                var update = true // used to redraw screen ONLY when needed (when rt is pressed)
-
-                while (true) {
-                    if (update) {
-                        clearAndWrite(city.name, true)
-                        LCD.newLine()
-                        LCD.write("${if (rt) 1 else 0}  * to Print")
-                        update = false
-                    }
-
-                    when (KBD.getKey()) {
-                        '*' -> {
-                            clearAndWrite(city.name, true)
-                            clearAndWrite("Collect Ticket", true, 1, 0, false)
-                            collectTicket(city.name, id, rt)
-                            origin = id
-                            clearAndWrite("Thank you", true)
-                            clearAndWrite("Have a nice trip", true, 1, 0, false)
-                            Time.sleep(1500)
-                            return@handleCitySelection
-                        }
-                        '0' -> {
-                            rt = !rt
-                            update = true
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    /**
-     * Prints and waits for ticket collect.
-     */
-    fun collectTicket(cityName: String, id: Int, rt: Boolean) {
+    fun collectTicketScreen(cityName: String, fn: () -> Unit) {
         clearAndWrite(cityName, true)
         clearAndWrite("Collect Ticket", true, 1, 0, false)
-        TicketDispenser.print(id, origin, rt)
-        Stations.addSold(cityName)
-        origin = id
-        while (SerialEmitter.isBusy()) {
-        }
+        fn()
         clearAndWrite("Thank you", true)
         clearAndWrite("Have a nice trip", true, 1, 0, false)
         Time.sleep(1500)
     }
 
-    /**
-     * Normal mode (buying ticket)
-     */
-    fun normalMode() {
-        handleCitySelection(false, true) { key, city, id ->
-            if (key == '#') {
-                var rt = false
-                var update = true // used to redraw screen ONLY when needed (when rt is pressed)
-                var credit = 0
-
-                var mult = 1 // multiply by 2 if RT (1 is default because no RT by default)
-                var finalPrice = mult * city.price
-
-                while (true) {
-                    if (update) {
-
-                        clearAndWrite(city.name, true)
-                        val price = centsToString(finalPrice - credit)
-                        clearAndWrite("${if (rt) 1 else 0}    $price$EURO_CHAR", l = 1, clear = false)
-                        update = false
-                    }
-
-                    // if a coin is inserted
-                    if (CoinAcceptor.hasCoin()) {
-                        update = true
-
-                        // insert coin and check if we can already buy the ticket
-                        val coin = CoinAcceptor.getCoinValue()
-                        CoinDeposit.insertCoin(coin)
-                        credit += coin
-                        CoinAcceptor.acceptCoin()
-
-                        if (credit >= finalPrice) {
-                            collectTicket(city.name, id, rt)
-                            CoinAcceptor.collectCoins()
-                            return@handleCitySelection
-                        }
-                    }
-
-                    // handle input
-                    when (KBD.getKey()) {
-                        '0' -> {
-                            rt = !rt
-                            update = true
-                            mult = if (rt) 2 else 1 // multiply by 2 if RT
-                            finalPrice = mult * city.price // update final price
-                        }
-                        '#' -> {
-                            if (credit < city.price) {
-                                clearAndWrite("Vending aborted", true)
-                                clearAndWrite(
-                                    "Return ${centsToString(credit)}$EURO_CHAR",
-                                    true,
-                                    l = 1,
-                                    clear = false
-                                )
-                                CoinDeposit.returnValue(credit)
-                                CoinAcceptor.ejectCoins()
-                                Time.sleep(1000)
-                                return@handleCitySelection
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
 }
 
 fun main() {
--- software/lic/src/main/kotlin/TicketMachine.kt
+++ software/lic/src/main/kotlin/TicketMachine.kt
@@ -1,9 +1,23 @@
+import isel.leic.utils.Time
+import java.text.SimpleDateFormat
+import java.util.*
+
 /**
- * Main app object
+ * Main APP object.
  */
 object TicketMachine {
+    // Used to know when to redraw waiting message
+    var updateWaitingScreen = false
+
+    // Our current city
+    private var origin = 0
+
+    // Updated date + format
+    private val DATEFORMAT = SimpleDateFormat("dd/MM/yyyy HH:mm")
+    private var updatedDate = DATEFORMAT.format(Date())
+
     /**
-     * Inits needed stuff
+     * Inits needed stuff for APP.
      */
     fun init() {
         HAL.init()
@@ -24,22 +38,22 @@ object TicketMachine {
      */
     fun run() {
         var shutdown = false
-        TUI.showWaitingScreen()
+        waitingScreen()
 
         // main loop
         while (!shutdown) {
-            TUI.showWaitingScreen()
+            waitingScreen()
 
             // maintenance mode
             if (M.verify()) {
-                TUI.updateWaitingScreen = false
-                shutdown = TUI.maintenanceMode()
+                updateWaitingScreen = false
+                shutdown = maintenanceModeScreen()
             }
 
             // buy ticket
             if (KBD.getKey() == '#') {
-                TUI.normalMode()
-                TUI.updateWaitingScreen = false
+                normalModeScreen()
+                updateWaitingScreen = false
             }
         }
 
@@ -49,8 +63,295 @@ object TicketMachine {
         LCD.clear()
         System.exit(1)
     }
+
+    /**
+     * Shows waiting screen.
+     */
+    private fun waitingScreen() {
+        val now = DATEFORMAT.format(Date())
+        if (now != updatedDate) {
+            updatedDate = now
+            updateWaitingScreen = false
+        }
+        if (!updateWaitingScreen) {
+            TUI.clearAndWrite("Ticket To Ride", true)
+            LCD.newLine()
+            LCD.write(updatedDate.toString())
+            updateWaitingScreen = true
+        }
+    }
+
+    /**
+     * Maintenance mode screen.
+     */
+    private fun maintenanceModeScreen(): Boolean {
+        val options = listOf(
+            "1-Print Ticket",
+            "2-Station Cnt.",
+            "3-Coins Cnt.",
+            "4-Reset Cnt.",
+            "5-Shutdown"
+        )
+        var idx = 0
+        var timer = Time.getTimeInMillis()
+
+        TUI.clearAndWrite("Maintenance mode")
+
+        do {
+            if (elapsed(timer) > 2500) {
+                if (idx == options.size - 1) idx = 0
+                else idx++
+                TUI.clearAndWrite("Maintenance mode")
+                timer = Time.getTimeInMillis()
+            }
+
+            LCD.cursor(1, 0)
+            LCD.write(options[idx])
+
+            when (KBD.getKey()) {
+                '1' -> testPrintTicket()
+                '2' -> maintenanceStationsScreen()
+                '3' -> maintenanceCoinsScreen()
+                '4' -> { // reset
+                    if (TUI.yesNoQuestion("Reset counters?", 5000L)) {
+                        Stations.resetStations()
+                        CoinDeposit.resetQuantity()
+                    }
+                }
+                '5' -> { // shutdown
+                    if (TUI.yesNoQuestion("Shutdown?", 5000L)) return true
+                    else continue
+                }
+            }
+        } while (M.verify())
+        return false
+    }
+
+    /**
+     * Simulates ticket printing (maintenance mode)
+     */
+    private fun testPrintTicket() {
+        handleCitySelection(false, true) { key, city, id ->
+            if (key == '#') {
+                var rt = false
+                var update = true // used to redraw screen ONLY when needed (when rt is pressed)
+
+                while (true) {
+                    if (update) {
+                        TUI.clearAndWrite(city.name, true)
+                        TUI.toPrintText(rt)
+                        update = false
+                    }
+
+                    when (KBD.getKey()) {
+                        '*' -> {
+                            TUI.collectTicketScreen(city.name) {
+                                TicketDispenser.print(id, origin, rt)
+                                while (SerialEmitter.isBusy());
+                            }
+                            return@handleCitySelection
+                        }
+                        '0' -> {
+                            rt = !rt
+                            update = true
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Coins screen (maintenance mode)
+     */
+    private fun maintenanceCoinsScreen() {
+        val coins = CoinDeposit.coins.keys.toList()
+        var arrowMode = false
+        var keyIdx = 0
+        val toCent = listOf(5, 10, 20, 50, 100, 200)
+
+        TUI.writeCoins(toCent[keyIdx], CoinDeposit.coins[coins[keyIdx]] ?: 0, arrowMode, keyIdx)
+        while (true) {
+            val key = KBD.waitKey(5000)
+            if (key == KBD.NONE.toChar() || key == '#') {
+                break
+            }
+
+            if (key == '*') {
+                arrowMode = !arrowMode
+                TUI.writeCoins(toCent[keyIdx], CoinDeposit.coins[coins[keyIdx]] ?: 0, arrowMode, keyIdx)
+                continue
+            }
+
+            // handle arrow mode
+            if ((key == '2' || key == '8') && arrowMode) {
+                val up = key == '2'
+                when {
+                    up && keyIdx < 5 -> keyIdx++
+                    !up && keyIdx > 0 -> keyIdx--
+                    else -> continue
+                }
+                TUI.writeCoins(toCent[keyIdx], CoinDeposit.coins[coins[keyIdx]] ?: 0, arrowMode, keyIdx)
+                continue
+            }
+
+            // handle normal mode
+            if (key in '0'..'5') {
+                keyIdx = key - '0'
+                TUI.writeCoins(toCent[keyIdx], CoinDeposit.coins[coins[keyIdx]] ?: 0, arrowMode, keyIdx)
+            }
+        }
+    }
+
+    /**
+     * Stations screen (maintenance mode)
+     */
+    private fun maintenanceStationsScreen() {
+        handleCitySelection(true, false) { _, _, _ -> }
+    }
+
+    /**
+     * Normal mode (buying ticket).
+     */
+    private fun normalModeScreen() {
+        handleCitySelection(false, true) { key, city, id ->
+            if (key == '#') {
+                var rt = false
+                var update = true // used to redraw screen ONLY when needed (when rt is pressed)
+                var credit = 0
+
+                var mult = 1 // multiply by 2 if RT (1 is default because no RT by default)
+                var finalPrice = mult * city.price
+
+                while (true) {
+                    if (update) {
+                        TUI.writeCityRtPrice(city.name, rt, finalPrice)
+                        update = false
+                    }
+
+                    // if a coin is inserted
+                    if (CoinAcceptor.hasCoin()) {
+                        update = true
+
+                        // insert coin and check if we can already buy the ticket
+                        val coin = CoinAcceptor.getCoinValue()
+                        CoinDeposit.insertCoin(coin)
+                        credit += coin
+                        CoinAcceptor.acceptCoin()
+
+                        if (credit >= finalPrice) {
+                            TUI.collectTicketScreen(city.name) {
+                                TicketDispenser.print(id, origin, rt)
+                                while (SerialEmitter.isBusy());
+                                Stations.addSold(city.name)
+                                origin = id
+                            }
+
+                            CoinAcceptor.collectCoins()
+                            return@handleCitySelection
+                        }
+                    }
+
+                    // handle input
+                    when (KBD.getKey()) {
+                        '0' -> {
+                            rt = !rt
+                            update = true
+                            mult = if (rt) 2 else 1 // multiply by 2 if RT
+                            finalPrice = mult * city.price // update final price
+                        }
+                        '#' -> {
+                            if (credit < city.price) {
+                                TUI.vendingAbortedScreen(credit) {
+                                    CoinDeposit.returnValue(it)
+                                    CoinAcceptor.ejectCoins()
+                                }
+                                return@handleCitySelection
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Handles city selection and executes [fn] block.
+     * The function returns if [returnIfHashtag] is true and '#' is pressed.
+     * [showPrice] will show the price on the right side, if false, shows how many tickets were sold.
+     * **This can be useful to avoid duplicate code.**
+     * Lambda char is key pressed, String is city object, Int is city ID
+     */
+    fun handleCitySelection(
+        returnIfHashtag: Boolean, showPrice: Boolean, fn: (Char, Stations.City, Int) -> Unit
+    ) {
+        val cities = Stations.cities
+        var keyIdx = 0
+        var arrowMode = false
+
+        // Writes to LCD city correctly formatted
+        fun writeCity() {
+            TUI.clearAndWrite(cities[keyIdx].name, true)
+            LCD.newLine()
+            val str = String.format("%02d${if (arrowMode) ARROW_CHAR else ":"}", keyIdx)
+            LCD.write(str)
+            if (!showPrice) {
+                LCD.cursor(1, 14)
+                LCD.write(String.format("%02d", cities[keyIdx].sold))
+            } else {
+                LCD.cursor(1, 11)
+                val price = centsToString(cities[keyIdx].price)
+                LCD.write("$price$EURO_CHAR")
+            }
+        }
+
+        // loop and show cities
+        writeCity()
+        while (true) {
+            val key = KBD.waitKey(5000)
+            if (key == KBD.NONE.toChar()) {
+                break
+            }
+
+            if (key == '#' && returnIfHashtag)
+                break
+
+            if (key == '*') {
+                arrowMode = !arrowMode
+                writeCity()
+                continue
+            }
+
+            // handle arrow mode
+            if ((key == '2' || key == '8') && arrowMode) {
+                val up = key == '2'
+                when {
+                    up && keyIdx != cities.size - 1 -> keyIdx++
+                    !up && keyIdx > 0 -> keyIdx--
+                    else -> continue
+                }
+                writeCity()
+                continue
+            }
+
+            // handle normal mode
+            if ((key - '0') < cities.size && key != '#') {
+                // check if we can "autocomplete" 2 digit
+                // i.e press 1 and then 2 gets us 12, but if 12 does not exist, gets 2
+                val n = String.format("%d%d", keyIdx, key - '0').toInt()
+                keyIdx = if (n < cities.size)
+                    n
+                else
+                    key - '0'
+                writeCity()
+            }
+
+            fn(key, cities[keyIdx], keyIdx)
+        }
+    }
 }
 
+//
 fun main() {
     println("TicketMachine App by Pedro & Roberto")
     TicketMachine.init()
